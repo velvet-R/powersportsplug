7463795c01478d894bdd13d9e9ba8f9bb7043af5
ichami630 <brandonichami@gmail.com>
2026-05-28 05:28:34 +0100

    fix root app routing

--- next.config.ts
+++ next.config.ts
@@ -49,9 +49,9 @@ const nextConfig: NextConfig = {
     }
     return webpackConfig
   },
-  turbopack: {
-    root: path.resolve(dirname),
-  },
+  // turbopack: {
+  //   root: path.resolve(dirname),
+  // },
 }
 
 export default withPayload(nextConfig)
