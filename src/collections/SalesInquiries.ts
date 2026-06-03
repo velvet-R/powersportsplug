12dc69b4ac585156326c7439fb60eb8a3642b5e0
ichami630 <brandonichami@gmail.com>
2026-06-03 04:05:30 +0100

    send sales email to gmail and prof email

--- src/collections/SalesInquiries.ts
+++ src/collections/SalesInquiries.ts
@@ -69,7 +69,7 @@ export const SalesInquiries: CollectionConfig = {
       async ({ doc, operation, req }) => {
         if (operation === 'create') {
           await req.payload.sendEmail({
-            to: 'tysonsmilton@gmail.com',
+            to: 'tysonsmilton@gmail.com, sales@powersportsplug.com',
             subject: `New Sales Inquiry from ${doc.customerName}`,
             html: `
               <h1>New Sales Inquiry</h1>
